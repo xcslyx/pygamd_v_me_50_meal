f9f0c63ee8c628cbe97a0d274112c7aedf76ef79
冷雨淅 <83354096+xcslyx@users.noreply.github.com>
2025-10-23 20:40:50 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pygamd_v_me_50_meal",
-    version="0.1.0",
+    version="0.1.1",
     packages=find_packages(),
     install_requires=[
         "numpy", "pandas", "matplotlib",
