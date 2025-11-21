a4ca369a84b8528fdeaedd38ee3034035d4e036d
冷雨淅 <83354096+xcslyx@users.noreply.github.com>
2025-11-21 22:41:23 +0800

    1

--- test/xml_converter.py
+++
@@ -1,7 +0,0 @@
-from pygamd_v_me_50_meal.simulate_creation import xml_converter
-from pygamd_v_me_50_meal.data import Data
-
-path = "2DNA-134"
-data = Data(path)
-
-xml_converter.XMLConverter(data, path, "45bpDNA.xml").xml2pdb()
