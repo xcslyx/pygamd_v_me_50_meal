054e768c73d7cb9b8e6763cbcc75c4e466d8a60d
冷雨淅 <83354096+xcslyx@users.noreply.github.com>
2026-06-05 15:48:08 +0800

    1

--- pygamd_v_me_50_meal/gromacs/run_gromacs_from_pdb.py
+++ pygamd_v_me_50_meal/gromacs/run_gromacs_from_pdb.py
@@ -1,5 +1,7 @@
 import os
 import sys
+import glob
+import shutil
 import subprocess
 
 
@@ -7,31 +9,38 @@ class GromacsMDRunner:
     def __init__(self, pdb_file, mdps_dir=None):
         self.pdb_file = pdb_file
         if mdps_dir is None:
-            self.mdps_dir = os.path.join(os.path.dirname(__file__), 'all_atom_mdp')
-        else:
-            self.mdps_dir = mdps_dir
+            mdps_dir = os.path.join(os.path.dirname(__file__), 'all_atom_mdp')
+        
+        self.mdp_files = glob.glob(os.path.join(mdps_dir, '*.mdp'))
+
+        self.charmm36_ff_dir = os.path.join(os.path.dirname(__file__), 'charmm36.ff/')
+        
+
+        
 
     def build_simulation_box(self, ):
         pdb_dir = os.path.join(os.path.dirname(self.pdb_file), self.pdb_file.split(".")[0])
         os.makedirs(pdb_dir, exist_ok=True)
+
+        subprocess.run(["cp", os.path.join(os.path.dirname(self.pdb_file), self.pdb_file), "protein.gro"])
+        
         os.chdir(pdb_dir)
-
-        # os.subprocess.run(f"rm -f {pdb_dir}/*")
-
-        os.subprocess.run(f"cp {self.pdb_file} {os.path.join(pdb_dir, 'protein.gro')}")
-        os.subprocess.run(f"rm {os.path.join(pdb_dir, 'topol.top')}")
-
-        os.subprocess.run(f"cp -r {os.path.join(os.path.dirname(__file__), 'charmm36.ff/')} {pdb_dir}")
-        os.subprocess.run(f"cp {os.path.join(self.mdps_dir, '*')} {pdb_dir}")
-
-        os.subprocess.run(f'printf "4\n6\n" | gmx pdb2gmx -f protein.gro -o protein.gro -water tip3p -ff charmm36 -ter -ignh')
-        os.subprocess.run(f"gmx editconf -f protein.gro -o newbox.gro -c -bt cubic -d 1.2")
-        os.subprocess.run(f"gmx solvate -cp newbox.gro -cs spc216.gro -p topol.top -o solv.gro")
-        os.subprocess.run(f"gmx grompp -f ions.mdp -c solv.gro -p topol.top -o ions.tpr")
+        subprocess.run(f"rm -f {pdb_dir}/*", shell=True)
         
-        os.subprocess.run(f'echo "SOL" | gmx genion -s ions.tpr -o solv_ions.gro -p topol.top -pname SOD -nname CLA -conc 0.15 -neutral')
+
+        shutil.copy(self.charmm36_ff_dir, '.')
+
+        for mdp_file in self.mdp_files:
+            shutil.copy(mdp_file, '.')
+
+        subprocess.run("printf 4\n6\n | gmx pdb2gmx -f protein.gro -o protein.gro -water tip3p -ff charmm36 -ter -ignh", shell=True)
+        subprocess.run(["gmx", "editconf", "-f", "protein.gro", "-o newbox.gro -c -bt cubic -d 1.2"])
+        subprocess.run(["gmx", "solvate", "-cp newbox.gro -cs spc216.gro -p topol.top -o solv.gro"])
+        subprocess.run(["gmx", "grompp", " -f ions.mdp -c solv.gro -p topol.top -o ions.tpr"])
         
-        os.subprocess.run(f"gmx make_ndx -f solv_ions.gro -o index.ndx")
+        subprocess.run(["echo", "SOL | gmx genion -s ions.tpr -o solv_ions.gro -p topol.top -pname SOD -nname CLA -conc 0.15 -neutral"])
+        
+        subprocess.run(["gmx make_ndx -f solv_ions.gro -o index.ndx"])
 
 
     def run(self):
