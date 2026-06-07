8cc24b3cb33dc27c3bb7c97e0a25729091f16787
冷雨淅 <83354096+xcslyx@users.noreply.github.com>
2026-06-07 17:03:00 +0800

    1

--- pygamd_v_me_50_meal/gromacs/run_gromacs_from_pdb.py
+++ pygamd_v_me_50_meal/gromacs/run_gromacs_from_pdb.py
@@ -4,6 +4,8 @@ import glob
 import shutil
 import subprocess
 
+from Bio.PDB import PDBParser
+
 
 class GromacsMDRunner:
     def __init__(self, pdb_file, mdps_dir=None):
@@ -16,9 +18,45 @@ class GromacsMDRunner:
 
         self.charmm36_ff_dir = os.path.join(os.path.dirname(__file__), 'charmm36.ff/')
 
+
+    @staticmethod
+    def analyze_molecule_type(pdb_file):
+        # 标准残基集合
+        protein_residues = {
+            'ALA', 'ARG', 'ASN', 'ASP', 'CYS', 
+            'GLN', 'GLU', 'GLY', 'HIS', 'ILE', 
+            'LEU', 'LYS', 'MET', 'PHE', 'PRO', 
+            'SER', 'THR', 'TRP', 'TYR', 'VAL'
+        }
+        # RNA标准残基 (包含一些旧版或常见的修饰写法)
+        rna_residues = {'A', 'U', 'C', 'G', 'ADE', 'URA', 'CYT', 'GUA'}
+        # DNA标准残基 (仅作对比参考)
+        dna_residues = {'DA', 'DT', 'DC', 'DG'}
+        
+        parser = PDBParser(QUIET=True)
+        structure = parser.get_structure('molecule', pdb_file)
+
+        # 遍历结构中的所有残基
+        molecule_dict = {"protein": False, "rna": False, "dna": False}
+        for residue in structure.get_residues():
+            # 获取残基名称并去除可能存在的空格
+            res_name = residue.get_resname().strip().upper()
+            
+            if res_name in protein_residues:
+                molecule_dict["protein"] = True
+                break
+            elif res_name in rna_residues:
+                molecule_dict["rna"] = True
+                break
+            elif res_name in dna_residues:
+                molecule_dict["dna"] = True
+
+        return next((key for key, value in molecule_dict.items()), None)
+
+
     def build_simulation_box(self, ):
         os.makedirs(self.pdb_dir, exist_ok=True)
-        subprocess.run(f"rm -f {pdb_dir}/*", shell=True)
+        subprocess.run(f"rm -f {self.pdb_dir}/*", shell=True)
 
         subprocess.run(["cp", self.pdb_file, os.path.join(self.pdb_dir, "protein.pdb")])
         os.chdir(self.pdb_dir)
@@ -29,7 +67,12 @@ class GromacsMDRunner:
 
         pdb2gmx = ["gmx", "pdb2gmx", "-f", "protein.pdb", "-o", "protein.gro",
             "-water", "tip3p", "-ff", "charmm36", "-ter", "-ignh"]
-        subprocess.run(pdb2gmx, input="2\n1\n", text=True, check=True)
+        molecule_type = self.analyze_molecule_type(self.pdb_file)
+        if molecule_type == "protein":
+            pdb2gmx_input = "2\n1\n"
+        elif molecule_type == "rna":
+            pdb2gmx_input = "4\n6\n"
+        subprocess.run(pdb2gmx, input=pdb2gmx_input, text=True, check=True)
         
         edit_conf = ["gmx", "editconf", "-f", "protein.gro", "-o", "newbox.gro",
             "-c", "-bt", "cubic", "-d", "1.2"]
@@ -52,13 +95,13 @@ class GromacsMDRunner:
 
 
     def run_simulation(self):
-        os.chdir(self.pdb_dir)
+        # os.chdir(self.pdb_dir)
 
         grompp_minim = ["gmx", "grompp", "-f", "minim.mdp", "-c", "solv_ions.gro",
             "-p", "topol.top", "-o", "em.tpr", "-maxwarn", "1", "-n", "index.ndx"]
         subprocess.run(grompp_minim)
         
-        mdrun_minim = ["gmx", "mdrun", "-v", "-deffnm", "em", "-nt", "16"]
+        mdrun_minim = ["gmx", "mdrun", "-v", "-deffnm", "em", "-nt", "16", "-ntmpi", "1"]
         subprocess.run(mdrun_minim)
         
         grompp_nvt = ["gmx", "grompp", "-f", "nvt.mdp", "-c", "em.gro", "-r", "em.gro",
