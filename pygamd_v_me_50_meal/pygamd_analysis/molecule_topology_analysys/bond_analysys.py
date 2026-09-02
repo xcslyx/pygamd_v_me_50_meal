46d36694f3b1f2cbe8e03b60fde76e046ea11f77
冷雨淅 <83354096+xcslyx@users.noreply.github.com>
2026-09-02 09:08:06 +0800

    增加了统计键长、键角、二面角的功能

--- pygamd_v_me_50_meal/pygamd_analysis/molecule_topology_analysys/bond_analysys.py
+++ pygamd_v_me_50_meal/pygamd_analysis/molecule_topology_analysys/bond_analysys.py
@@ -29,13 +29,10 @@ class BondAnalysys:
         for bond in bond_dict:
             bond_length_dict[bond] = []
             for bond_pair in bond_dict[bond]:
-                bond_length_dict[bond].append(np.linalg.norm(position_list[bond_pair[0]] - position_list[bond_pair[1]]))
+                bond_length_dict[bond].append(float(np.linalg.norm(position_list[bond_pair[0]] - position_list[bond_pair[1]])))
         
         with open(os.path.join(self.bond_path, file_name), 'w') as f:
             f.write(str(bond_length_dict))    
-        
-        with open(os.path.join(self.sys_topol_path, "bond_length.txt"), 'w') as f:
-            f.write(str(bond_length_dict))
      
     def get_bond_length_parallel(self):
         files = os.listdir(self.xml_path)
@@ -49,12 +46,13 @@ class BondAnalysys:
                           bar_format='{l_bar}{bar}| {n_fmt}/{total_fmt} [{elapsed}<{remaining}, {rate_fmt}]',
                           ncols=100))
         
-        bond_length_dict = {}
+        merged_bond_length_dict = {}
         for file_name in os.listdir(self.bond_path):
             with open(os.path.join(self.bond_path, file_name), 'r') as f:
                 for bond_name, values in eval(f.read()).items():
-                    if bond_name not in bond_length_dict:
-                        bond_length_dict[bond_name] = []
-                    bond_length_dict[bond_name].extend(values)
+                    if bond_name not in merged_bond_length_dict:
+                        merged_bond_length_dict[bond_name] = []
+                    merged_bond_length_dict[bond_name].extend(values)
     
-    
+        with open(os.path.join(self.sys_topol_path, "bond_length.txt"), 'w') as f:
+            f.write(str(merged_bond_length_dict))
